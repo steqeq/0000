2346d939a86c0d306cb68cd3cccf7a23ed9da7df
Máté Ferenc Nagy-Egri <mate@streamhpc.com>
2023-03-28 14:03:54 +0200

    Disable MD005

--- mdlrc-style.rb
+++ mdlrc-style.rb
@@ -5,5 +5,6 @@ rule 'MD013', :line_length => 99999
 # Allow in-line HTML
 exclude_rule 'MD033'
 
-# MyST list-table syntax violates MD004
+# MyST list-table syntax violates MD004 and MD005
 exclude_rule 'MD004'
+exclude_rule 'MD005'
