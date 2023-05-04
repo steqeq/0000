9cc94ad07eb06c3cc8bbcbaaec3c8d73913c4738
Sam Wu <sam.wu2@amd.com>
2023-05-04 16:59:12 -0600

    remove alpha

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ article_pages = [
     {"file":"understand/isv_deployment_win", "os":["windows"]},
 ]
 
-docs_core = ROCmDocs("ROCm Docs 5.6.0 Alpha")
+docs_core = ROCmDocs("ROCm Docs 5.6.0")
 docs_core.setup()
 docs_core.disable_main_doc_link()
 
