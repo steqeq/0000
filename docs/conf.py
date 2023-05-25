0a8c13c9802baaf44507d8801d20f0f05504d2b0
Gergely Meszaros <gergely@streamhpc.com>
2023-05-25 13:35:19 +0000

    Replace links to subprojects docs with intersphinx links

--- docs/conf.py
+++ docs/conf.py
@@ -58,5 +58,7 @@ external_toc_path = "./sphinx/_toc.yml"
 docs_core = ROCmDocs("ROCm Documentation")
 docs_core.setup()
 
+external_projects_current_project = "rocm"
+
 for sphinx_var in ROCmDocs.SPHINX_VARS:
     globals()[sphinx_var] = getattr(docs_core, sphinx_var)
