e1fe746cfc2ffdac6aeda50cf1d58d6e368efeec
Sam Wu <sam.wu2@amd.com>
2023-06-30 09:21:06 -0600

    update pdf

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,8 @@ shutil.copy2('../CHANGELOG.md','./CHANGELOG.md')
 project = "ROCm Documentation"
 author = "Advanced Micro Devices, Inc."
 copyright = "Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved."
-version = "5.0.0"
-release = "5.0.0"
+version = "5.0.1"
+release = "5.0.1"
 
 setting_all_article_info = True
 all_article_info_os = ["linux"]
