235191535c52006b29fe03cfaa6ad3d427b7f54b
Sam Wu <22262939+samjwu@users.noreply.github.com>
2024-07-22 09:40:38 -0600

    Remove unused configurations in conf.py (#3444)

--- docs/conf.py
+++ docs/conf.py
@@ -5,25 +5,10 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import shutil
-import jinja2
-import os
 
-# Environment to process Jinja templates.
-jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader("."))
-
-# Jinja templates to render out.
-templates = []
-
-# Render templates and output files without the last extension.
-# For example: 'install.md.jinja' becomes 'install.md'.
-for template in templates:
-    rendered = jinja_env.get_template(template).render()
-    with open(os.path.splitext(template)[0], 'w') as file:
-        file.write(rendered)
-
-shutil.copy2('../RELEASE.md','./about/release-notes.md')
 # Keep capitalization due to similar linking on GitHub's markdown preview.
-shutil.copy2('../CHANGELOG.md','./about/changelog.md')
+shutil.copy2("../RELEASE.md", "./about/release-notes.md")
+shutil.copy2("../CHANGELOG.md", "./about/changelog.md")
 
 latex_engine = "xelatex"
 latex_elements = {
@@ -46,44 +31,54 @@ all_article_info_author = ""
 
 # pages with specific settings
 article_pages = [
+    {"file": "about/release-notes", "os": ["linux", "windows"], "date": "2024-06-04"},
+    {"file": "about/changelog", "os": ["linux", "windows"], "date": "2024-06-04"},
+    {"file": "how-to/deep-learning-rocm", "os": ["linux"]},
+    {"file": "how-to/gpu-enabled-mpi", "os": ["linux"]},
+    {"file": "how-to/system-debugging", "os": ["linux"]},
+    {"file": "how-to/tuning-guides", "os": ["linux", "windows"]},
+    {"file": "how-to/rocm-for-ai/index", "os": ["linux"]},
+    {"file": "how-to/rocm-for-ai/install", "os": ["linux"]},
+    {"file": "how-to/rocm-for-ai/train-a-model", "os": ["linux"]},
+    {"file": "how-to/rocm-for-ai/deploy-your-model", "os": ["linux"]},
+    {"file": "how-to/rocm-for-ai/hugging-face-models", "os": ["linux"]},
+    {"file": "how-to/rocm-for-hpc/index", "os": ["linux"]},
+    {"file": "how-to/llm-fine-tuning-optimization/index", "os": ["linux"]},
+    {"file": "how-to/llm-fine-tuning-optimization/overview", "os": ["linux"]},
     {
-        "file":"about/release-notes",
-        "os":["linux", "windows"],
-        "date":"2024-06-04"
+        "file": "how-to/llm-fine-tuning-optimization/fine-tuning-and-inference",
+        "os": ["linux"],
     },
     {
-        "file":"about/changelog",
-        "os":["linux", "windows"],
-        "date":"2024-06-04"
+        "file": "how-to/llm-fine-tuning-optimization/single-gpu-fine-tuning-and-inference",
+        "os": ["linux"],
     },
-
-    {"file":"how-to/deep-learning-rocm", "os":["linux"]},
-    {"file":"how-to/gpu-enabled-mpi", "os":["linux"]},
-    {"file":"how-to/system-debugging", "os":["linux"]},
-    {"file":"how-to/tuning-guides", "os":["linux", "windows"]},
-
-    {"file":"how-to/rocm-for-ai/index", "os":["linux"]},
-    {"file":"how-to/rocm-for-ai/install", "os":["linux"]},
-    {"file":"how-to/rocm-for-ai/train-a-model", "os":["linux"]},
-    {"file":"how-to/rocm-for-ai/deploy-your-model", "os":["linux"]},
-    {"file":"how-to/rocm-for-ai/hugging-face-models", "os":["linux"]},
-
-    {"file":"how-to/rocm-for-hpc/index", "os":["linux"]},
-
-    {"file":"how-to/llm-fine-tuning-optimization/index", "os":["linux"]},
-    {"file":"how-to/llm-fine-tuning-optimization/overview", "os":["linux"]},
-    {"file":"how-to/llm-fine-tuning-optimization/fine-tuning-and-inference", "os":["linux"]},
-    {"file":"how-to/llm-fine-tuning-optimization/single-gpu-fine-tuning-and-inference", "os":["linux"]},
-    {"file":"how-to/llm-fine-tuning-optimization/multi-gpu-fine-tuning-and-inference", "os":["linux"]},
-    {"file":"how-to/llm-fine-tuning-optimization/llm-inference-frameworks", "os":["linux"]},
-    {"file":"how-to/llm-fine-tuning-optimization/model-acceleration-libraries", "os":["linux"]},
-    {"file":"how-to/llm-fine-tuning-optimization/model-quantization", "os":["linux"]},
-    {"file":"how-to/llm-fine-tuning-optimization/optimizing-with-composable-kernel", "os":["linux"]},
-    {"file":"how-to/llm-fine-tuning-optimization/optimizing-triton-kernel", "os":["linux"]},
-    {"file":"how-to/llm-fine-tuning-optimization/profiling-and-debugging", "os":["linux"]},
+    {
+        "file": "how-to/llm-fine-tuning-optimization/multi-gpu-fine-tuning-and-inference",
+        "os": ["linux"],
+    },
+    {
+        "file": "how-to/llm-fine-tuning-optimization/llm-inference-frameworks",
+        "os": ["linux"],
+    },
+    {
+        "file": "how-to/llm-fine-tuning-optimization/model-acceleration-libraries",
+        "os": ["linux"],
+    },
+    {"file": "how-to/llm-fine-tuning-optimization/model-quantization", "os": ["linux"]},
+    {
+        "file": "how-to/llm-fine-tuning-optimization/optimizing-with-composable-kernel",
+        "os": ["linux"],
+    },
+    {
+        "file": "how-to/llm-fine-tuning-optimization/optimizing-triton-kernel",
+        "os": ["linux"],
+    },
+    {
+        "file": "how-to/llm-fine-tuning-optimization/profiling-and-debugging",
+        "os": ["linux"],
+    },
 ]
-
-exclude_patterns = ['temp']
 
 external_toc_path = "./sphinx/_toc.yml"
 
@@ -99,10 +94,6 @@ html_css_files = ["rocm_custom.css"]
 
 html_title = "ROCm Documentation"
 
-html_theme_options = {
-    "link_main_doc": False
-}
+html_theme_options = {"link_main_doc": False}
 
-redirects = {
-    "reference/openmp/openmp": "../../about/compatibility/openmp.html"
-}
+redirects = {"reference/openmp/openmp": "../../about/compatibility/openmp.html"}
